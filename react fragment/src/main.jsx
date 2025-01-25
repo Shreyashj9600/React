8ffefce5fa55f601bf3ef01daf7cedc3c1def8ae
Shreyashj9600 <connectshreyashj96@gmail.com>
2025-01-25 12:36:51 +0530

    fragment

---
+++ react fragment/src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDom from 'react-dom'
+
+ReactDom.render(
+    <>
+        <h1>Netflix Pick</h1>
+        <p>Hear the list of my fav 5 Netflix series </p>
+        <ol>
+            <li>Dark</li>
+            <li>Extra curricular</li>
+            <li>my holo love</li>
+            <li>my first -2 love</li>
+            <li>Mr Robot </li>
+        </ol>
+    </>,
+    document.getElementById('root')
+)
